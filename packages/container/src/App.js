b49ddf4e14da795a3c1e0eeb7f44d2ae002a27ec
DevAlberto <devalbertobarboza@gmail.com>
2021-02-27 15:33:50 -0300

    randomiz production classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,16 +2,22 @@ import React from 'react';
 import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
 import { BrowserRouter as Router, Route, NavLink, BrowserRouter } from 'react-router-dom'
+import { createGenerateClassName, StylesProvider } from '@material-ui/core/styles';
 
-
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co'
+})
 
 export default () => {
-  return( 
-    <BrowserRouter >
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+  return(
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter >
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
+    
   );
 };
