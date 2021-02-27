74b4c2c966df8b2ee3de369ce2fc447e2d51b558
DevAlberto <devalbertobarboza@gmail.com>
2021-02-27 19:27:07 -0300

    multi-tier feature added

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,12 +1,31 @@
 import { mount } from "marketing/MarketingApp";
 import React, { useEffect, useRef } from 'react';
+import { useHistory } from "react-router-dom";
+
 
 export default () => {
+  /**
+   * This ref is to grab the DOM HTMLElement reference to render the app
+   */
   const ref = useRef(null)
 
+  const history = useHistory();
+
   useEffect(() => {
-    mount(ref.current)
-  }, [ref])
+    const { onParentNavigate } = mount(ref.current, {
+      /**
+       * @function onNavigate updates the history browser with the new pathname if the condition is met
+       * @param {nextPathname} nextPathname is the pathname provided by memory history object
+       * which we will use to sync with the container path
+       */
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location;
+        if(pathname !== nextPathname) return history.push(nextPathname);
+      }
+    });
+
+    history.listen(onParentNavigate);
+  },[]);
 
   return <div ref={ref} />
 }
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,22 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./App";
+import { createMemoryHistory, createBrowserHistory } from 'history';
 
-
-const mount = (el) => {
+/**
+ * @param {el} el is the DOM reference whre we want to render the app
+ * @param {onNavigate} onNavigate is a Function
+ * @fires ReachDom.render with the App component
+ * @returns onParentNavigate function to use for browser history updating
+ */
+const mount = (el, {onNavigate, defaultHistory }) => {
+    const history = defaultHistory || createMemoryHistory();
+    if(onNavigate) history.listen(onNavigate);
     ReactDOM.render(
-        <App />,
+        <App  history={history} />,
         el
     );
+    return {
+        /**
+       * @function onParentNavigate 
+       * @param {nextPathname} nextPathname is the pathname provided by memory history object
+       * which we will use to sync with the container path
+       * @returns the history browser with the new pathname if the condition is met
+       */
+        onParentNavigate({ pathname: nextPathname }) {
+            const { pathname } = history.location;
+            if(pathname !== nextPathname) return history.push(nextPathname);
+        }
+    }
 };
-//If we are in development and in isolation
-// call mount immediately
+
+/**
+ * if we are in development and in isolation call mount immediately
+ */
 if (process.env.NODE_ENV === 'development' ){
     const devRoot = document.querySelector('#_marketing-dev-root')
     if ( devRoot ) {
-        mount( devRoot );
+        /**
+         * we are providing browser history for development mode
+         */
+        mount( devRoot, { defaultHistory: createBrowserHistory() } );
     }
 }
 
-// we are running through container and we should export the mount function
+/**
+ * we are running through container and we should export the mount function 
+*/
 export { mount };
